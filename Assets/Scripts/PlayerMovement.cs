f408af367a85fecccdac945ef1cf636949178976
CIITSHSDTP00\CIIT <cicerobarasi@gmail.com>
2025-03-10 18:33:18 +0800

    as
    asdasd

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator anim;
+
+    private Vector2 input;
+    public float moveSpeed = 4f;
+    public float maxVelocityChange = 10f;
+    public bool crouching;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        input.Normalize();
+
+        if (input.magnitude > 0)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouching = !crouching;
+        }
+
+        if (crouching)
+        {
+            anim.SetBool("Crouch", true);
+        }
+        else
+        {
+            anim.SetBool("Crouch", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0,500,0));
+            
+        }
+
+        if (rb.linearVelocity.y != 0)
+        {
+            anim.SetBool("Jump", true);
+        }
+        else
+        {
+            anim.SetBool("Jump", false);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(CalculateMovement(moveSpeed), ForceMode.VelocityChange);
+    }
+
+    Vector3 CalculateMovement(float _speed)
+    {
+        Vector3 targetVelocity = new Vector3(input.x, 0, input.y);
+        targetVelocity = transform.TransformDirection(targetVelocity);
+
+        targetVelocity *= _speed;
+
+        Vector3 velocity = rb.linearVelocity;
+
+        if (input.magnitude > 0.5f)
+        {
+            Vector3 velocityChange = targetVelocity - velocity;
+
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            return velocityChange;
+
+            
+        }
+
+        // Return zero movement if input magnitude is too low
+        return new Vector3();
+    }
+}
